6418d2acb83beb8a70b7002649ced68ac4bcff6c
nthuyzzz14 <mathanhthuyreal207@gmail.com>
2026-05-30 20:01:23 +0700

    Java-basic

--- hethongquanlyBenhVien/BenhVien.java
+++ hethongquanlyBenhVien/BenhVien.java
@@ -2,22 +2,112 @@ package hethongquanlyBenhVien;
 
 import java.util.ArrayList;
 public class BenhVien {
-
-  ArrayList <NhanVienYTe> danhSachNV = new ArrayList <NhanVienYTe>();
-
-  ArrayList <BenhNhan> danhSachBN = new ArrayList <BenhNhan>();
+  protected String ten;
+  protected ArrayList <NhanVienYTe> danhSachNV = new ArrayList <NhanVienYTe>();
+  protected ArrayList <BenhNhan> danhSachBN = new ArrayList <BenhNhan>();
   
-  public void themBS(){
-
+  public BenhVien(String ten){
+    this.ten = ten;
+    this.danhSachBN = new ArrayList<>();
+    this.danhSachNV = new ArrayList<>();
+  }
+  // QUAN LY NHAN VIEN
+  public void themNhanVien(NhanVienYTe nv){
+    danhSachNV.add(nv);
+    System.out.println("Da them nhan vien: "+nv.gethoTen()+" ("+nv.getClass().getSimpleName()+")");
   }
 
-  public void themYT(){
-
+ //QUAN LY BENH NHAN
+  public void tiepnhanBenhNhan(BenhNhan bn, CoTheTiepNhan nguoicothetiepnhan){
+    danhSachBN.add(bn);
+    nguoicothetiepnhan.tiepNhan(bn);
   }
 
-  public void tiepNhanBN(){
-
+  public BenhNhan timBN(String maBN){
+    for(BenhNhan bn : danhSachBN){
+      if (bn.getmaBN().equals(maBN))
+        return bn;
+    }
+    return null;
   }
 
-  
+  public void xuatVienBenhNhan(String maBN, CoTheTiepNhan nguoicothexuatvien){
+    BenhNhan ma = timBN(maBN);
+    if(ma != null){
+      nguoicothexuatvien.xuatVien(maBN);
+      danhSachBN.remove(ma);
+      System.out.println("Benh nhan: "+ma.gethoTen()+" da xuat vien !");
+    }
+    else {
+      System.out.printf("Khong tim thay benh nhan co ma [%s] phu hop !", maBN);
+    }
+  }
+
+  //in bao coa danh sach 
+  public void indanhSachNV(){
+    System.out.println("\n======== DANH SACH NHAN IEN Y TE ========"+ten);
+
+    int soBS = 0;
+    int soYT = 0;
+    for(NhanVienYTe nv : danhSachNV){
+      if(nv instanceof BacSi){
+        soBS++;
+      } else if (nv instanceof YTa){
+        soYT++;
+      }
+    }
+    System.out.println("\n---------------------------------");
+    System.out.printf("| Tong so nhan vien: %d",danhSachNV.size());
+    System.out.printf("\n| So ba si hien co: %d", soBS);
+    System.out.printf("\n| So y ta hien co: %d", soYT);
+  }
+
+  public void indanhSachBenhNhan(){
+    System.out.println("\n======== DANH SACH BENH NHAN DANG DIEU TRI =======");
+
+    if (danhSachBN.isEmpty()){
+      System.out.println("Hien khong co benh nhan nao dang dieu tri");
+      return;
+    }
+    int bnnoiTru = 0;
+    int bnngoaiTru = 0;
+    for (BenhNhan bn : danhSachBN){
+      String loaiBenhNhan = (bn instanceof BenhNhanNoiTru) ? "[Noi Tru]" : "[Ngoai Tru]";
+      System.out.println("| "+loaiBenhNhan+"| "+bn);
+      if (bn instanceof BenhNhanNoiTru){
+        bnnoiTru++;} 
+        else{
+        bnngoaiTru++;}
+    }
+    System.out.println("\n==========================================");
+    System.out.printf("| Tong so benh nhan: %d", danhSachBN.size());
+    System.out.printf("\n| Benh nhan noi tru: %d", bnnoiTru);
+    System.out.printf("\n| Benh nhan ngoai tru: %d", bnngoaiTru);
+  }
+  // thong ke vien phi
+  public void thongkeVienPhi(){
+    System.out.println("\n========== THONG KE VIEN PHI ==========");
+    
+    double tong = 0;
+    for (BenhNhan bn : danhSachBN){
+      System.out.println("%-25s -> %,12.0f VND%n");
+      tong += bn.tinhVienPhi();
+    }
+    System.out.println("\n=======================================");
+    System.out.printf("| Tong doanh thu vien phi: %,15.0f VNĐ%n", tong);
+  }
+
+  // thuc hien da hinh cho obj
+  public void congViec(){
+    System.out.println("\n========== BAT DAU CA LAM VIEC ==========");
+    for (NhanVienYTe nv : danhSachNV){
+      nv.lamViec();
+    }
+  }
+
+  // ten cua benh vien
+  public String getTen(){
+    return ten;
+  }
+
 }
--- hethongquanlyBenhVien/main.java
+++ hethongquanlyBenhVien/main.java
@@ -1,9 +1,64 @@
 package hethongquanlyBenhVien;
 
-import java.util.Scanner;
-import java.util.ArrayList;
 public class main {
   public static void main(String[] args){
-    Scanner nhap = new Scanner(System.in);
+
+    System.out.println("__________ HE THONG QUAN LY BENH VIEN ____________");
+
+    // bao ten cua benh vien
+    BenhVien bv = new BenhVien("\nBENH VIEN DA KHOA TAN LONG");
+
+    // khai bao cac nhan vien y te : bac si va y ta
+    BacSi bs1 = new BacSi("Ma Thanh Thuy", "T01", "Noi soi", 40000000, "Thac Si", 201);
+    BacSi bs2 = new BacSi("Ngo Kien Huy", "H02", "Ngoai soi", 40000000, "Thac Si", 202);
+    YTa y01 = new YTa("L01", "Le Luu Ly", "Phu san", "Tien si", 202, 15000000);
+    YTa y02 = new YTa("A01", "Duong Ngoc Anh", "Tu Van", "Tien si", 201, 15000000);
+    
+    // add vao giup hien thi thong bao nhan vien duoc nhan vao cong ty
+    System.out.println("\n======== DANH SACH NHAN VIEN MOI ========");
+    bv.themNhanVien(bs1);
+    bv.themNhanVien(bs2);
+    bv.themNhanVien(y01);
+    bv.themNhanVien(y02);
+
+    BenhNhanNoiTru bn1 = new BenhNhanNoiTru("b01", "Nguyen Van Chien", "12", "Dau bung, di ngoai ra mau", 20, 301, 500000, "27");
+    BenhNhanNoiTru bn2 = new BenhNhanNoiTru("b02", "Nguyen Van Vinh", "23", "Dau dau , buon non", 50, 304, 7000000, "13");
+    BenhNhanNgoaiTru bn3 = new BenhNhanNgoaiTru("b03", "Duoong The Anh", "28", "Bung bia ,da bong hay", "10h20", "201", 600000);
+
+    bv.tiepnhanBenhNhan(bn1, y02);
+    bv.tiepnhanBenhNhan(bn3, bs1);
+    bv.tiepnhanBenhNhan(bn2, bs2);
+
+    bv.congViec();
+
+    System.out.println("\n========= KET QUA KHAM BENH ===========");
+    bs1.kham(bn3);
+    System.out.println("Ket qua kham: "+ bs1.ketQua());
+
+    bs2.kham(bn2);
+    System.out.println("Ket qua kham: "+bs2.ketQua());
+
+    bs1.kham(bn1);
+    System.out.println("Ket qua kham: "+bs1.ketQua());
+
+    System.out.println("\n-------------- HO TRO BAC SI -------------");
+    y01.lamViec();
+    y02.lamViec();
+
+    //thong ke vien phi 
+    bv.thongkeVienPhi();
+
+    //xuat vien cho benh nhan
+    System.out.println("---------------- XUAT VIEN ------------");
+    bs1.xuatVien("bn3");
+
+    //in cac danh sach 
+    bv.indanhSachNV();
+    bv.indanhSachBenhNhan();
+    bv.thongkeVienPhi();
+
+
+    System.out.println("------------ KET THUC CHUONG TRINH ------------");
+
   }
 }  
